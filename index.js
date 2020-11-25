b578d63ce8ed653be7244f35a8ddd35596a232e1
JColombo <juan.colombo95@gmail.com>
2020-11-25 20:54:02 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var pokemonImg = document.querySelector('.pokemonImg')
+var pokemonName = document.querySelector('.pokemonName');
+
+const urlParams = new URLSearchParams(window.location.search);
+const myParam = urlParams.get('name');
+
+async function getPokemonData(myParam) {
+    const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${myParam}`)
+        .then((res) => res.json())
+        .then((data) => (obj = data))
+        .catch((error) => console.log(error));
+    return data
+}
+
+async function set() {
+    const data = await getPokemonData(myParam);
+    if (data) {
+        pokemonImg.src = data.sprites.front_default;
+        var name = data.name.charAt(0).toUpperCase() + data.name.slice(1)
+        pokemonName.innerText = name;
+    } else {
+        pokemonImg.removeAttribute('src')
+        pokemonName.innerText = "Pokemon not found";
+    }
+
+}
+
+console.log(getPokemonData(myParam))
+set()
